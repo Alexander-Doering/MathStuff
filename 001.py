a70f44ae9bc8c595ea346396fd947beae7313ace
Alexander Doering <alexander-doering@outlook.de>
2024-09-17 14:36:02 +0200

    Functional Version

--- 001.py
+++ 001.py
@@ -2,14 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import string
 import time
-import math
 timestart=time.time()
-alpha_dict = dict(enumerate(string.ascii_uppercase))
-PointInput =  np.array([[50,50],[-30,40],[70,-20],[-60,-30],[30,-10],[-34,-10]])
-# Unit Vector Function
-def unitvec(vec1):
-    return vec1/np.linalg.norm(vec1)
+alpha_dict = dict(enumerate(string.ascii_letters))
+#n=10 for tests PointInput =  np.array([[50,50],[-30,40],[70,-20],[-60,-30],[10,24],[25,-20],[-30,35],[-5,-13],[-40,-12],[12,30]])
+#n=52 (Currently limited by the alpha_dict size )
+PointInput = np.array([[160,828],[243,988],[-573,829],[-166,504],[-860,991],[-911,-572],[-330,399],[524,965],[-792,274],[-508,124],[766,966],[-375,482],[550,904],[-657,744],[-154,662],[-373,765],[-677,-428],[839,864],[-626,-17],[58,568],[-967,-538],[-342,295],[-810,-585],[-441,948],[675,704],[-745,-280],[100,808],[-495,-46],[-823,574],[-597,668],[411,603],[-410,211],[-489,861],[-738,-661],[-71,414],[792,946],[-981,-458],[34,72],[-601,331],[503,807],[-298,-280],[-766,612],[466,849],[110,956],[93,950],[-84,54],[-841,-415],[-32,945],[330,976],[594,619],[-769,-621],[-694,866]])
 
+# Defines which part of the Coordinate System the points are in, used for relation.
+#TODO: Allow x,y=0 and add calc for that
+def CoordinateQuarter(x,y):
+    if x > 0 and y > 0:
+        return 1
+    elif x > 0 and y < 0: 
+        return 2
+    elif x < 0 and y > 0:
+        return 4
+    elif x < 0 and y < 0:
+        return 3
 # Draw Line between 2 Points.
 def DrawConnection(x1,y1,x2,y2):
     plt.plot([x1,x2],[y1,y2],'k-')
@@ -37,44 +46,86 @@ def Connector(Dictionary):
             else:
                 x2 = Dictionary[b][0]
                 y2 = Dictionary[b][1]
-                print(x,y,x2,y2)
                 DrawConnection(x,y,x2,y2)
-# Calculate Angle between 2 Vectors    
-def AngleCalc(vec1,vec2):
-    return np.arccos(np.clip(np.dot(unitvec(vec1),unitvec(vec2)), -1.0,1.0))
-
-# Generates Vectors for Each point to each point.
-def genVectors(Dictionary):
-    VecList=dict()
-    for a in Dictionary:   
-        v1 = Dictionary[a]
-        for b in Dictionary:
-            if a == b: continue
+# Takes 3 Tuples and Computes function of the first two, and related the third to it
+def functionRelation(p1,p2,p3):
+    dx = p1[0]-p2[0]
+    dy = p1[1]-p2[1]
+    if dx != 0 and dy != 0:
+        b = -(dy/dx * p1[0] - p1[1])
+        fy = dy/dx * p3[0] + b
+    else: #This means its a straight line.
+        if dx == 0: #Straight Line where all y are in val. You can now calculate it directly
+            if p3[0] > p1[0]:
+                rel=-1
+            elif p3[0] < p1[0]:
+                rel=1
+            else: 
+                print("Stopping Now. Point:"+ p3 , +" is not linearly independant")
+                return 0
+            if p2[1] > p1[1]:
+                return (rel*-1)
             else:
-                v2 = Dictionary[b]
-                name=a+b
-                VecList[name]=[v1[0]-v2[0],v1[1]-v2[1]]
-    return(VecList)
-
-# Generate all Angles for every possible pair of vectors except XY <-> YX
-def genAngles(Dictionary):
-    angleList=dict()
-    for a in Dictionary:   
-        point1=a[:1]
-        point2=a[1:2]
-        for b in Dictionary:
-            if a==b: continue
-            elif a[:1] != b[:1]: continue
+                return(rel)
+        else: # This means it's a straight line paralel to the x-axis
+            if p3[1] > p1[1]:
+                rel=-1
+            elif p3[1] < p1[1]:
+                rel=1
             else:
-                point3=b[1:2]
-                angle=round((AngleCalc(Dictionary[a],Dictionary[b])/math.pi)*180,2)
-                name=a+"->"+b     
-                print(name, " Angle: ",  angle,"°")
-     
+                print("Stopping Now. Point:"+ p3 , +" is not linearly independant")
+                return 0
+            if p2[0] > p1[0]:
+                return(rel*-1)
+            else:
+                return(rel)
+    if fy > p3[1]:
+        rel= -1
+    elif fy == p3[1]:
+        print("Stopping Now. Point:"+ p3 , +" is not linearly independant")
+        return 0
+    else:
+        rel = 1
+    # Makes the middle point of between p1 & p2 the middle of the coordinate system
+    mx=(p1[0]+p2[0])/2
+    my=(p1[1]+p2[1])/2
+    # Defines which part of the Coordinate System the points are in 
+    p1q = CoordinateQuarter(p1[0]-mx, p1[1]-my)
+    p2q = CoordinateQuarter(p2[0]-mx, p2[1]-my)
+    if ((p1q == 1 and p2q == 3) or (p1q == 2 and p2q == 4)):
+        mod = 1
+    elif ((p1q == 3 and p2q == 1) or (p1q == 4 and p2q == 2)):
+        mod = -1
+    else:
+        mod = 0
+        print(" Coord System Error, you done fucked up")
+        print(p1,p2)
+        print(mx,my)
+        print((p1[0]-mx, p1[1]-my),(p2[0]-mx, p2[1]-my))
+        print(p1q,p2q)
+        exit(3)
+    return (rel*mod)
+   
+def RelationGenerator(Dictionary):
+    rellist=dict()
+    for a in Dictionary:
+        for b in Dictionary:
+            if(a == b):
+                continue
+            else:
+                for c in Dictionary:
+                    if a == c or b == c:
+                        continue
+                    print(a,b,c)
+                    val=functionRelation(Dictionary[a],Dictionary[b],Dictionary[c])
+                    rellist[a,b,c]=val
+    return rellist
 PList=PointPlotter(PointInput)
 Connector(PList)
-VList=genVectors(PList)
-genAngles(VList)
+RList=RelationGenerator(PList)
+#print("Ergebnis:")
+#print(RList)
+print("Menge an ergebnissen:", len(RList) )
 timeend=time.time()
 print(timeend-timestart)
 plt.show()
--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Things I've made to explain math to myself or because i was bored
 
 ToC:
 #001 
-Script for analyzing points in $\mathbb{R}^2$ regarding their Relation to eachother.
+Script for analyzing points in $\mathbb{Z}^2$ regarding their Relation to eachother.
 #002
 #003
 #004
